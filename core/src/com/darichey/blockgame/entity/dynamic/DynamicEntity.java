30692fb9463ce3519dd68d1cb3cbececf9d71121
grandmind <darichey1@gmail.com>
2016-05-19 22:32:39 -0500

    DynamicEntity#isOnGround() should now work properly

--- core/src/com/darichey/blockgame/entity/dynamic/DynamicEntity.java
+++ core/src/com/darichey/blockgame/entity/dynamic/DynamicEntity.java
@@ -3,6 +3,8 @@ package com.darichey.blockgame.entity.dynamic;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.darichey.blockgame.entity.Entity;
+import com.darichey.blockgame.entity.block.Block;
+import com.darichey.blockgame.init.Blocks;
 import com.darichey.blockgame.world.World;
 
 public class DynamicEntity extends Entity {
@@ -41,6 +43,19 @@ public class DynamicEntity extends Entity {
 	}
 
 	public boolean isOnGround() {
-		return (getPosition().y == (int) getPosition().y) && (world.getBlockAt(new Vector2(getPosition().x, getPosition().y - 1)) != null);
+		// At most, the player can be standing on two blocks at once. This is true if one of them is solid and can be walked on.
+		boolean isStandingOnSolid;
+		
+		// If the player is standing on two blocks, we need to check both. Otherwise, we just need to check the one directly below him
+		if (Math.floor(getPosition().x) != Math.ceil(getPosition().x)) {
+			Block leftBlock = world.getBlockAt(new Vector2((int)Math.floor(getPosition().x), getPosition().y - 1));
+			Block rightBlock = world.getBlockAt(new Vector2((int)Math.ceil(getPosition().x), getPosition().y - 1));
+
+			isStandingOnSolid = (leftBlock != null) || (rightBlock != null);
+		} else {
+			isStandingOnSolid = (world.getBlockAt(new Vector2((int)getPosition().x, getPosition().y - 1)) != null);
+		}
+
+		return (getPosition().y == Math.round(getPosition().y)) && isStandingOnSolid;
 	}
 }
